1c7df1858183f01834b00bca58764a4b04b3e9ed
Angel Ebebi <99768121+a-ebebi@users.noreply.github.com>
2022-06-26 05:10:10 -0400

    Delete emotionAlly directory

--- emotionAlly/main.py
+++
@@ -1,8 +0,0 @@
-# import and run flask application
-
-from website import create_app 
-
-app = create_app()
-
-if __name__ == '__main__': # only execute if running file not just import
-  app.run(debug=True) # for development not production
